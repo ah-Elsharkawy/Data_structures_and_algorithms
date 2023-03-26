5dc4a1d894fb8066736b32ef6f6d96b1180b87de
AhmedElsharkawy <aelsharkawy56@gmail.com>
2023-03-26 16:43:05 +0200

    Merge_sort

--- cpp/merge_sort.cpp
+++ cpp/merge_sort.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// merge two sorted vectors v1 and v2 in one victor v
 void merge(vector<int> &v, vector<int> v1, vector<int> v2)
 {
     int i=0, j=0, k=0;
@@ -35,6 +36,10 @@ void merge(vector<int> &v, vector<int> v1, vector<int> v2)
    
 }
 
+// divide the given vector v into two vectors and the two resulting vectors are divided also 
+// and so on until it reaches one element per vector then merge the resulting vectors using merge function
+// Time complexity: O(nlog(n))
+// Space complexity: O(n) where n is the number of items in the vector
 void merge_sort(vector<int> &v)
 {
     if(v.size() <= 1)
